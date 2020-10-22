806a45bf574550f9ac0afc9b8ebe1e0b6eb9d2a3
unknown <mitesh@aiisma.com>
2020-10-22 21:28:16 +0530

    Added Jaeles Project

--- shaurya/shaurya.sh
+++ shaurya/shaurya.sh
@@ -61,16 +61,22 @@ amass enum --passive -d $1 -o $2-amass.txt
 sleep 2
 
 echo -e "${BOLD}\nCombining and Filtering Unique Subdomains in Target-finalsubdomains.txt $1... \n ${NORMAL} "
-cat $2-sublister.txt $2-assetfinder.txt $2-subfinder.txt $2-amass.txt | grep -v "*" |sort -u > $2-finalsubdomains.txt
+cat $2-sublister.txt $2-assetfinder.txt $2-subfinder.txt $2-amass.txt | sort -u | uniq -u > $2-finalsubdomains.txt
 sleep 2
 
-echo -e "${NORMAL}${BOLD}\nSubdomain Scan Finished starting HTTPX & Filtering $1${NORMAL}${BOLD}... \n"
+
+
+############################################SUBDOMAIN SCAN FINISHED##########################################
+
+
+
+echo -e "${NORMAL}Starting ${GREEN}HTTPx${NORMAL} on all filtered subdomains..."
+cat $2-finalsubdomains.txt | httpx -silent -follow-redirects | sort -u | uniq -u > $2-alive.txt
 sleep 2
 
 echo -e "${NORMAL}Starting ${GREEN}HTTPx${NORMAL} on all filtered subdomains..."
-cat $2-finalsubdomains.txt | sort -u | uniq -u | httpx -silent -follow-redirects | sort -u | uniq -u > $2-alive.txt
-sleep 2
-
+cat $2-finalsubdomains.txt | while read domain;do dig  $domain;done | tee -a $2-digs.txt
+cat $2-digs.txt | grep CNAME > $2-cnames.txt
 
 echo -e "${NORMAL}Starting ${GREEN}Checking Live domains for Subdomain Takeover using Subzy,Subjack,Nuclei${NORMAL} on $1..."
 subjack -w $2-finalsubdomains.txt -t 20 -ssl -c ~/tools/fingerprints.json -o $2-subjack.txt
@@ -88,24 +94,33 @@ echo -e "${NORMAL}Eyewitness Snapshot of each Live subdomain ${NORMAL} ..."
 python3 ~/tools/EyeWitness/Python/EyeWitness.py -f $2-alive.txt --no-prompt --web --timeout 15 
 sleep 2
 
-echo -e "${NORMAL}Starting ${GREEN}Nuclei CVE Scan${NORMAL} alive domains after updating templates..."
-nuclei -update-templates
+echo -e "${NORMAL}Naabu Portscan without Cloudflare on Live subdomain ${NORMAL} ..."
+cat $2-finalsubdomains.txt | filter-resolved | cf-check | sort -u | naabu -rate 40000 -silent -verify | httprobe > $2-naabuports.txt
+sleep 2
+
+
+###################################Finsihed Playing with Subdomains##########################################
+
+
+
+
+echo -e "${NORMAL}Starting ${GREEN}Nuclei CVE Scan${NORMAL}"
 cat $2-alive.txt | nuclei -t ~/tools/nuclei-templates/cves -o $2-nucleiCVES.txt
 sleep 2
 
-echo -e "${NORMAL}Starting ${GREEN}Nuclei Tokens Scan on ${NORMAL} alive domains"
+echo -e "${NORMAL}Starting ${GREEN}Nuclei Tokens Scan on ${NORMAL}"
 cat $2-alive.txt | nuclei -t ~/tools/nuclei-templates/tokens -o $2-nucleiTokens.txt
 sleep 2
 
-echo -e "${NORMAL}Starting ${GREEN}Nuclei Vulnerablities Scan on ${NORMAL} alive domains"
+echo -e "${NORMAL}Starting ${GREEN}Nuclei Vulnerablities Scan on ${NORMAL}"
 cat $2-alive.txt | nuclei -t ~/tools/nuclei-templates/vulnerabilities -o $2-nucleiVulnerabilities.txt
 sleep 2
 
-echo -e "${NORMAL}Starting ${GREEN}Nuclei Files Scan on ${NORMAL} alive domains"
+echo -e "${NORMAL}Starting ${GREEN}Nuclei Files Scan on ${NORMAL}"
 cat $2-alive.txt | nuclei -t ~/tools/nuclei-templates/files -o $2-nucleiFiles.txt
 sleep 2
 
-echo -e "${NORMAL}Starting ${GREEN}Nuclei Basic LFI Fuzzing Scan on ${NORMAL} alive domains"
+echo -e "${NORMAL}Starting ${GREEN}Nuclei Basic LFI Fuzzing Scan on ${NORMAL}"
 cat $2-alive.txt | nuclei -t ~/tools/nuclei-templates/fuzzing -o $2-nucleiLfi.txt
 sleep 2
 
@@ -113,43 +128,121 @@ echo -e "${NORMAL}Starting ${GREEN}Nuclei Default Credentials Scan on ${NORMAL} 
 cat $2-alive.txt | nuclei -t ~/tools/nuclei-templates/default-credentials -o $2-nucleiDefaultCred.txt
 sleep 2
 
-echo -e "${NORMAL}${BOLD}Starting ${GREEN}Waybackurls${NORMAL}${BOLD} param output on $1... ${NORMAL} "
-cat $2-alive.txt | waybackurls | tee $2-waybackurls.txt
+echo -e "${NORMAL}Starting ${GREEN}Jaeles CVE Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/cves -U $2-alive.txt -o $2-jaelesCVE.txt
+#cat $2-alive.txt | anew | httpx -silent -threads 500 | xargs -I@ jaeles scan -s /root/.jaeles/base-signatures -u @ > $2-jaeles.txt
 sleep 2
 
-#echo -e "${NORMAL}${BOLD}Starting ${GREEN}Gospider${NORMAL}${BOLD} param output on $1... ${NORMAL} "
-#gospider -S $2-finalsubdomains.txt -o $2-gospider -c 10 -d 1 && grep -r -o -E "(([a-zA-Z][a-zA-Z0-9+-.]*\:\/\/)|mailto|data\:)([a-zA-Z0-9\.\&\/\?\:@\+-\_=#%;,])*" outdir | sort -u | tee $2-gospider.txt
-echo -e "${NORMAL}${BOLD}Starting ${GREEN}Hackcrawler${NORMAL}${BOLD} param output on $1... ${NORMAL} "
-cat $2-alive.txt | hakrawler | tee $2-hakrawler.txt
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles Common Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/common -U $2-alive.txt -o $2-jaelesCommon.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles Sensitive Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/sensitive -U $2-alive.txt -o $2-jaelesSensitive.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles probe Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/probe -U $2-alive.txt -o $2-jaelesProbe.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/COMMON Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/common -U $2-alive.txt -o $2-jaelesFuzzCommon.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/CORS Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/cors -U $2-alive.txt -o $2-jaelesFuzzCors.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/CRLF Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/crlf -U $2-alive.txt -o $2-jaelesCRLF.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/LFI Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/lfi -U $2-alive.txt -o $2-jaelesLFI.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/open-redirect Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/open-redirect -U $2-alive.txt -o $2-jaelesOpenRedirect.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/randomfuzz Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/random -U $2-alive.txt -o $2-jaelesRandomFuzz.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/sqli Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/sqli -U $2-alive.txt -o $2-jaelesSqli.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles FUZZ/ssti Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/base-signatures/fuzz/ssti -U $2-alive.txt -o $2-jaelesSsti.txt
+sleep 2
+
+
+echo -e "${NORMAL}Starting ${GREEN}Jaeles PassiveKeys Scan on ${NORMAL} alive domains"
+jaeles scan -c 50 -s /root/.jaeles/passives -U $2-alive.txt -o $2-jaelesPassive.txt
+sleep 2
+
+
+
+###################################Finsihed Nuclei and Jaeles##########################################
+
+
+
+echo -e "${NORMAL}${BOLD}Starting ${GREEN}Waybackurls${NORMAL}${BOLD} param output on $1... ${NORMAL} "
+cat $2-alive.txt | waybackurls | tee $2-waybackurls.txt
 sleep 2
 
 echo -e "${NORMAL}${BOLD}Starting ${GREEN}Gau (Get all Urls)${NORMAL}${BOLD} param output on $1... ${NORMAL} "
 sleep 2
 cat $2-alive.txt | gau | tee $2-gau.txt
-cat $2-waybackurls.txt $2-gau.txt $2-hakrawler.txt | sort -u | uniq -u | $2-allurls.txt #Combining Results of Gospider,Wayback,Gau
+cat $2-waybackurls.txt $2-gau.txt | grep -v .jpg|grep -v .jpeg|grep -v .gif|grep -v .css|grep -v .tif|grep -v .tiff|grep -v .png|grep -v .ttf|grep -v .woff|grep -v .woff2|grep -v .ico|grep -v .pdf|grep -v .svg|grep -v .txt | sort -u | uniq -u | urlive -c 50 -o $2-allurls.txt #Combining Results of Wayback,Gau
 
 
-#echo -e "Starting ${GREEN}Dalfox${NORMAL} for XSS on $1... ${NORMAL}"
-#gospider -S $2-alive.txt -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)" -a | grep -e "code-200" | awk '{print $5}' | grep -v ".js?ver=" | grep -v ".js?version=" | grep -v ".css?ver=" | grep "=" | qsreplace -a | dalfox pipe -blind randommeet.xss.ht -o $2-dalfoxXSSgospider.txt
-#cat $2-allurls.txt | grep -v ".js?ver=" | grep -v ".css?ver=" | grep -v ".js?version=" | grep "=" | qsreplace -a | dalfox pipe -blind randommeet.xss.ht -o $2-dalfoxXSSgauway.txt
-#sleep 2
-
-echo -e "Starting ${GREEN}Kxss${NORMAL} for XSS on $1... ${NORMAL}"
-cat $2-allurls.txt | kxss > $2-kxss.txt
+###################################Fetched ALL URLS##########################################
 
 
-echo -e "Starting ${GREEN}REDIRECT${NORMAL} param filtering on $1... ${NORMAL} "
+echo -e "Starting ${GREEN}RCE${NORMAL} param filtering on $1... ${NORMAL} "
 sleep 2
-cat $2-allurls.txt | gf redirect > $2-redirect.txt
+cat $2-allurls.txt | gf rce > $2-rce.txt
 
 
-echo -e "Starting ${GREEN}AWS S3 Buckets${NORMAL} param filtering on $1... ${NORMAL} "
+echo -e "Starting ${GREEN}Interesting Params${NORMAL} param filtering on $1... ${NORMAL} "
 sleep 2
-cat $2-allurls.txt | gf s3-buckets | sort -u | uniq -u | tee -a $2-s3buckets.txt
+cat $2-allurls.txt | gf interestingparams > $2-interestingparams.txt
 
-echo -e "Starting ${GREEN}AWS Keys${NORMAL} param filtering on $1... ${NORMAL} "
+echo -e "Starting ${GREEN}Debug File GF Pattern${NORMAL} param filtering on $1... ${NORMAL} "
 sleep 2
-cat $2-allurls.txt | gf aws-keys | tee -a $2-awskeys.txt
+cat $2-allurls.txt | gf debug_logic > $2-debug_logic.txt
+
+
+echo -e "Starting ${GREEN}JS Link Finding${NORMAL} for on $1... ${NORMAL}"
+subjs -i $2-allurls.txt > $2-jsfiles
+
+echo -e "Starting ${GREEN}XSS without GF via qsreplace${NORMAL} on $1... ${NORMAL} "
+sleep 2
+cat $2-allurls.txt |grep '=' | qsreplace '"><script>alert(1)</script>' | while read host do ; do curl -s --path-as-is --insecure "$host" | grep -qs "<script>alert(1)</script>" && echo "$host \033[0;31m" Vulnerable;done | tee -a $2-XSSqsreplace.txt
+
+echo -e "Starting ${GREEN}SSTI qsreplace${NORMAL} on $1... ${NORMAL} "
+sleep 2
+cat $2-allurls.txt |grep '=' | qsreplace "xssfound{{7*7}}"  | httpx -match-regex 'xssfound81' -threads 300 -http-proxy http://127.0.0.1:8080 | tee -a $2-SSTIqsreplace.txt
+
+echo -e "Starting ${GREEN}XSS oneliner with Dalfox${NORMAL} on $1... ${NORMAL} "
+sleep 2
+gospider -S $2-allurls.txt -c 10 -d 5 --blacklist ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)" --other-source | grep -e "code-200" | awk '{print $5}'| grep "=" | qsreplace -a | dalfox pipe -o $2-XSSDalfox.txt
+
+echo -e "Starting ${GREEN}SQLInjection Check${NORMAL} for on $1... ${NORMAL}"
+cat $2-allurls.txt |grep '=' |qsreplace "' OR '1" | httpx -silent -threads 100 | grep -q -rn "syntax\|mysql" output 2>/dev/null && \printf "TARGET \033[0;32mCould Be Exploitable\e[m\n" || printf "TARGET \033[0;31mNot Vulnerable\e[m\n" > $2-sqlinjection.txt
 
 find . -type f -size 0 -delete
 echo -e "${BOLD}\nAll your outputs are saved in ${GREEN}$2/ \n"
